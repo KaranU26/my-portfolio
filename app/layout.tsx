886ee1f8991ce962173e596b464d429a6b1aeadf
Karandeep Ubhi <karan_u26@hotmail.com>
2024-05-21 15:55:59 -0400

    Added NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import HeroSection from "@/components/hero";
+import NavBar from "@/components/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <HeroSection />
+        <NavBar />
         {children}
       </body>
     </html>
