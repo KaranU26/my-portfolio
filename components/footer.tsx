cff5716c52d7f0e9bccebd527221bfcf511308e3
Karandeep Ubhi <karan_u26@hotmail.com>
2024-05-22 13:12:24 -0400

    Removed duplicate imports by autofill

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,6 @@
 
 import Link from "next/link"
 import { JSX, SVGProps } from "react"
-import { JSX, SVGProps } from "react"
-import { JSX, SVGProps } from "react"
 
 export default function Footer() {
   return (
