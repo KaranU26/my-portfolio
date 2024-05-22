42b2084e6e5507473ce2c16e9d8aa27b5d58e53b
Karandeep Ubhi <karan_u26@hotmail.com>
2024-05-22 12:49:00 -0400

    Added some front end code for the home page

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,11 +1,24 @@
-
+'use client'
 import { Button } from "@/components/ui/button"
 import { SheetTrigger, SheetContent, Sheet } from "@/components/ui/sheet"
 import Link from "next/link"
+import { useState } from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function NavBar() {
+    
+    const { setTheme } = useTheme()
+
   return (
-    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 drop-shadow">
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 shadow-bottom">
       <Sheet>
         <SheetTrigger asChild>
           <Button className="lg:hidden" size="icon" variant="outline">
@@ -34,11 +47,11 @@ export default function NavBar() {
           </div>
         </SheetContent>
       </Sheet>
-      <Link className="mr-6 hidden lg:flex" href="#">
-        {" ICON HERE"}
+      <Link className="mr-6 hidden lg:flex scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-2xl bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent" href="#">
+        Karan.dev
         <span className="sr-only">Acme Inc</span>
       </Link>
-      <nav className="ml-auto hidden lg:flex gap-6">
+      <nav className="ml-auto hidden lg:flex gap-6 items-center">
         <Link
           className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
           href="#"
@@ -63,6 +76,26 @@ export default function NavBar() {
         >
           Contact
         </Link>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
       </nav>
     </header>
   )
